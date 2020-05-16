4a1f9beaf18b76d01263420539dcd84ee1fa9b08
AlexSAsin <vip.casin@gmail.com>
2020-05-16 15:32:37 +0300

    comm

--- group_functions.sql
+++ group_functions.sql
@@ -1,89 +1,52 @@
---№1. Вывести всеми возможными способами имена и фамилии студентов, средний балл которых от 4 до 4.5
+--№1--
+select n_group, count(n_group)
+from students
+group by n_group
 
-select st.name, st.surname
+--№2--
+select n_group, max(score)
+from students
+group by n_group
+
+--№3--
+select surname, count(surname)
+from students
+group by surname
+
+--№5--
+select substr(st.n_group::varchar, 1, 1) as number, avg(st.score) as score
 from students st
-where st.score >= 4 and st.score <= 4.5
+group by substr(st.n_group::varchar, 1, 1)
 
-select st.name, st.surname
-from students st
-where not st.score < 4 and not st.score > 4.5
+--№6--
+SELECT st.n_group, MAX(st.score)
+FROM students st
+GROUP BY st.n_group
+LIMIT 1
 
-select st.name, st.surname
-from students st
-where st.score between 4 and 4.5
+--№7--
+SELECT st.n_group, AVG(st.score) AS avg
+FROM students st
+GROUP BY st.n_group
+HAVING AVG(st.score) <= 3.5
+ORDER BY avg DESC
 
---№2. Познакомиться с функцией CAST. Вывести при помощи неё студентов заданного курса (использовать Like)
+--№8--
+SELECT st.n_group, AVG(st.score) AS avg, MAX(st.score) AS max, MIN(st.score) AS min, COUNT(st.score) AS count
+FROM students st
+GROUP BY st.n_group
 
-select st.name, st.surname
-from students st
-where st.n_group::varchar like '2%'
+--№9--
+SELECT *
+FROM students st
+WHERE st.score= (SELECT MAX(st.score)
+				 FROM students st)
 
---№3. Вывести всех студентов, отсортировать по убыванию номера группы и имени от а до я
-
-select st.name, st.surname, st.n_group
-from students st
-order by st.n_group desc, st.name
-
---№4. Вывести студентов, средний балл которых больше 4 и отсортировать по баллу от большего к меньшему
-
-select st.name, st.surname, st.score
-from students st
-where st.score > 4
-order by st.score desc
-
---№5. Вывести на экран название и риск футбола и хоккея
-
-select hb.name, hb.risk
-from hobbies hb
-where hb.name = 'Футбол' or hb.name = 'Хоккей'
-
---№6. Вывести id хобби и id студента которые начали заниматься хобби между двумя заданными датами (выбрать самим) 
-      и студенты должны до сих пор заниматься хобби
-
-select sh.student_id, sh.hobby_id, sh.date_start, sh.date_finish
-from students_hobbies sh
-where (sh.date_start between '2018-01-01' and '2019-01-01') and sh.date_finish is null
-
---№7. Вывести студентов, средний балл которых больше 4.5 и отсортировать по баллу от большего к меньшему
-
-select st.name, st.surname, st.score
-from students st
-where st.score > 4.5
-order by st.score desc
-
---№8. Из запроса №7 вывести несколькими способами на экран только 5 студентов с максимальным баллом
-
-SELECT name, surname, score
-FROM students
-WHERE score =
-(SELECT max(score) FROM students)
-limit 5
-
-select st.name, st.surname, st.score
-from students st
-where st.score > 4.5
-order by st.score desc
-limit 5
-
---№9. Выведите хобби и с использованием условного оператора сделайте риск словами:
-        >=8 - очень высокий
-        >=6 & <8 - высокий
-        >=4 & <8 - средний
-        >=2 & <4 - низкий
-        <2 - очень низкий
-
-select name, case 
-when risk>=8 then 'очень высокий'
-when risk<8 and risk>=6 then 'высокий'
-when risk<6 and risk>=4 then 'средний'
-when risk<4 and risk>=2 then 'низкий'
-when risk<2 then 'очень низкий'
-end
-from hobbies
-
---№10. Вывести 3 хобби с максимальным риском
-
-select name
-from hobbies
-order by risk desc
-limit 3
+--№10--
+Select st.*
+From students st
+INNER Join(
+    Select st.n_group, max(st.score)
+    From students st
+    Group BY st.n_group) as t
+	ON st.n_group=t.n_group AND st.score=t.max(st.score)
--- multi-table_queries.sql
+++ multi-table_queries.sql
@@ -1,13 +1,11 @@
---1
-
+--1--
 SELECT st.name, st.surname, h.name as hobby
 From students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
 INNER JOIN hobbies h ON h.id=sh.hobby_id 
 GROUP BY st.name, st.surname, h.name
 
---2
-
+--2--
 SELECT st.name, st.surname, st.n_group, st.phone, st.city, st.score, st.birth_date, h.name as hobby, MAX(age(now(),sh.date_start)), sh.date_finish
 From students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
@@ -16,8 +14,7 @@ GROUP BY st.name, st.surname, st.n_group, st.phone, st.city, st.score, st.birth_
 ORDER BY MAX(age(now(),sh.date_start)) DESC
 LIMIT 1
 
---3
-
+--3--
 SELECT st.name, st.surname, st.birth_date, st.id, SUM(h.risk)
 FROM students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id  
@@ -25,16 +22,14 @@ INNER JOIN hobbies h ON h.id=sh.hobby_id
 WHERE st.score>(SELECT AVG(st.score) FROM students st) AND sh.date_finish is not null    
 GROUP BY st.name, st.surname, st.birth_date, st.id
 
---4
-
+--4--
 SELECT st.name, st.surname, st.birth_date, h.name as hobby, h.risk, extract(month from age(sh.date_finish, sh.date_start))+ extract (year from age (sh.date_finish, sh.date_start)*12)
 From students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
 INNER JOIN hobbies h ON h.id=sh.hobby_id 
 WHERE sh.date_finish IS NOT NULL
 
---5
-
+--5--
 SELECT st.name, st.surname, st.birth_date, st.id, extract(years from age(st.birth_date))
 FROM students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
@@ -43,16 +38,14 @@ WHERE sh.date_finish is null
 GROUP BY st.name, st.surname, st.birth_date, st.id
 HAVING extract(years from age(st.birth_date))=20
 
---6
-
+--6--
 SELECT DISTINCT(st.n_group),AVG(st.score)
 FROM students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
 WHERE sh.date_finish is null
 GROUP BY st.n_group
 
---7
-
+--7--
 SELECT h.name, h.risk, st.id, st.name, st.surname, sh.date_start,extract(month from age(sh.date_start))+ extract (year from age (sh.date_start)*12)
 FROM students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
@@ -62,8 +55,7 @@ GROUP BY h.name, h.risk, st.id, st.name, st.surname, sh.date_start, age(sh.date_
 ORDER BY MAX(age(now(),sh.date_start)) DESC
 LIMIT 1
 
---8
-
+--8--
 SELECT DISTINCT(h.name), MAX(s.score)
 FROM hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.hobby_id
@@ -72,8 +64,7 @@ WHERE s.score=(SELECT MAX(s.score)
 			  FROM students s)
 GROUP BY h.name, s.score
 
---9
-
+--9--
 SELECT h.name
 FROM hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.student_id 
@@ -81,8 +72,7 @@ INNER JOIN students s ON s.id=sh.hobby_id
 WHERE s.score BETWEEN 2.49 AND 3.5 AND sh.date_finish IS NULL AND CAST (s.n_group AS varchar) like '2___' 
 GROUP BY h.name, s.score
 
---10
-
+--10--
 SELECT *
 FROM (Select substr(st.n_group::varchar, 1,1) as course, count(*)
 From students st
@@ -97,8 +87,7 @@ Group by substr(st.n_group::varchar, 1,1)
 Having count (*)>1) as st_wh
 ON st_all.course = st_wh.course
 
---11
-
+--11--
 SELECT *
 FROM (Select substr(st.n_group::varchar, 1,4) as NGROUP, count(*) as c_group
 From students st
@@ -112,8 +101,7 @@ Having count (*)>1) as st_gd
 ON st_all.NGROUP = st_gd.NGROUP
 WHERE st_all.NGROUP::integer/st_gd.NGROUP::integer>=0.6
 
---12
-
+--12--
 Select substr(st.n_group::varchar, 1,1) as course, substr(sh.date_finish::varchar, 1,10) as finish, count(*) as c_good
 From students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
@@ -121,8 +109,7 @@ INNER JOIN hobbies h ON h.id=sh.hobby_id
 Group by substr(sh.date_finish::varchar, 1,10),substr(st.n_group::varchar, 1,1)
 Having count (*)>1
 
---13
-
+--13--
 Select st.id, st.name, st.surname, st.birth_date, substr(st.n_group::varchar, 1,1) as course
 From students st
 FULL JOIN students_hobbies sh ON st.id=sh.student_id
@@ -130,8 +117,7 @@ WHERE st.score >=4.5
 group by st.id, st.name, st.surname, st.birth_date, substr(st.n_group::varchar, 1,1)
 having count(sh.date_start)= count(sh.date_finish)
 
---14
-
+--14--
 CREATE OR REPLACE VIEW student_hobby AS
 SELECT st.* 
 FROM students st
@@ -139,8 +125,7 @@ INNer JOIN students_hobbies sh ON st.id=sh.student_id
 group by st.id, sh.date_finish
 having extract(year from age(now(), sh.date_finish))=5;
 
---15
-
+--15--
 Select h.name, count(st.name)
 from hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.hobby_id 
@@ -148,8 +133,7 @@ INNER JOIN students st ON st.id=sh.student_id
 WHERE sh.date_finish is null
 group by h.name
 
---16
-
+--16--
 Select h.id
 from hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.hobby_id 
@@ -159,8 +143,7 @@ group by h.id, h.name
 order by count(st.name) desc
 limit 1
 
---17
-
+--17--
 Select *
 From students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id  
@@ -175,7 +158,6 @@ Where h.name=(Select h.name
 				limit 1)
 
 --18
-
 Select h.id
 from hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.hobby_id 
@@ -184,8 +166,7 @@ group by h.id, h.risk
 order by h.risk desc
 limit 3
 
---19
-
+--19--
 Select *
 From students st
 Inner join students_hobbies sh ON st.id=sh.student_id
@@ -193,8 +174,7 @@ Where sh.date_finish is null
 Order by sh.date_start
 Limit 10
 
---20
-
+--20--
 Select DISTINCT(n_group)
 FROM(Select *
 		From students st
@@ -203,8 +183,7 @@ FROM(Select *
 		Order by sh.date_start
 		Limit 10) as st_all
 
---21
-
+--21--
 CREATE OR REPLACE VIEW student_hobby AS
 SELECT st.* 
 FROM students st
@@ -212,8 +191,7 @@ INNer JOIN students_hobbies sh ON st.id=sh.student_id
 group by st.id, sh.date_finish
 having extract(year from age(now(), sh.date_finish))=5;
 
---22
-
+--22--
 CREATE OR REPLACE VIEW student_hobby AS
 with c_hobby as(
 			select substr(st.n_group::varchar,1,1) as course, sh.hobby_id, count(*) as c
@@ -229,8 +207,7 @@ select *
 from max_for_course m_f_c
 inner join c_hobby c_h on c_h.course = m_f_c.course and m_f_c.max_c= c_h.c
 
---23
-
+--23--
 CREATE OR REPLACE VIEW student_hobby AS
 with c_hobby as(
 			select substr(st.n_group::varchar,1,1) as course, sh.hobby_id, count(*) as c
@@ -247,8 +224,7 @@ select *
 from max_for_course m_f_c
 inner join c_hobby c_h on c_h.course = m_f_c.course and m_f_c.max_c= c_h.c
 
---24
-
+--24--
 CREATE OR REPLACE VIEW student_hobby AS
 with all_students as(select substr(st.n_group::varchar,1,1) as course, count(distinct(st.name)) as all_course
 			        from students st
@@ -262,8 +238,7 @@ select *
 from all_students a_s
 full join good_students gs on a_s.course = gs.course
 
---25
-
+--25--
 Select h.*
 from hobbies h
 INNER JOIN students_hobbies sh ON h.id=sh.hobby_id 
@@ -273,36 +248,31 @@ group by h.id, h.name
 order by count(st.name) desc
 limit 1
 
---26
-
+--26--
 CREATE OR REPLACE VIEW V2 AS
 SELECT st.id, st.surname, st.name, st.n_group 
 FROM students st
 
---27
-
+--27--
 Select substr(st.name::varchar, 1,1) as alfavit, Max(st.score), avg(st.score), min(st.score)
 from students st
 where st.score>3.6
 group by alfavit
 order by alfavit
 
---28
-
+--28--
 select substr(st.n_group::varchar, 1,1) as course, st.surname, max(st.score), min(st.score), avg(st.score)
 from students st
 group by course, st.surname
 
---29
-
+--29--
 select substr(st.birth_date::varchar, 1,4) as year, count(h.name)
 from students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
 INNER JOIN hobbies h ON h.id=sh.hobby_id 
 group by year
 
---30
-
+--30--
 Select substr(st.name::varchar, 1,1) as alfavit, Max(h.risk), min(h.risk)
 from students st
 INNER JOIN students_hobbies sh ON st.id=sh.student_id 
@@ -310,8 +280,7 @@ INNER JOIN hobbies h ON h.id=sh.hobby_id
 group by alfavit
 order by alfavit
 
---31
-
+--31--
 SELECT extract(month from st.birth_date), avg(st.score)
 FROM students st
 INNER JOIN students_hobbies sh ON st.id = sh.student_id
@@ -319,45 +288,26 @@ INNER JOIN hobbies h ON h.id = sh.hobby_id
 WHERE h.name = 'Футбол'
 GROUP BY extract(month from st.birth_date)
 
---32
-
+--32--
 select distinct(st.name)|| ' ' || st.surname || ' ' || st.n_group as students
 from students st
 full JOIN students_hobbies sh ON st.id = sh.student_id
 where sh.date_start is not null
 
---33
-
+--33--
 SELECT st.surname,
 		case when position('ов' IN st.surname) = 0 then 'не найдено'
 				else position('ов' IN st.surname)::varchar end
 from students st
 
---34
-
+--34--
 sELECT rpad(st.surname,10,'--') 
 from students st
 
---35
-
+--35--
 SELECT trim('--' FROM n_surname) 
 FROM (sELECT rpad(st.surname,10,'--') as n_surname
 		from students st)as all_surname
 
---36
-
+--36--
 select '2000-05-01'::date -'2000-04-01'::date
-
---38
-
-SELECT EXTRACT(CENTURY FROM now ()) as century, EXTRACT(WEEK FROM now ()) as week, EXTRACT(DOY FROM now ()) as day
-
---39
-
-select st.name || ' ' || st.surname || ' ' || h.name || ' ' || case when sh.date_finish is null then 'занимается'
-																	when sh.date_finish is not null then 'закончил'
-																	end
-from students st
-inner join students_hobbies sh on sh.student_id=st.id
-inner join hobbies h on h.id=sh.hobby_id
-where sh.date_start is not null
